cd903e4c32570c011671b2788da6bf2c75a9a6e3
ChristoAW <99455915+ChristoAW@users.noreply.github.com>
2023-03-04 22:25:04 +0700

    add : popup announcement

--- src/data/list-divisi.js
+++ src/data/list-divisi.js
@@ -1,17 +1,161 @@
 const listDivisi = [
   { name: 'POSEIDON', link: '' },
-  { name: 'SIRENS', link: 'https://forms.gle/h96U2s9qztsvdQ3H9' },
-  { name: 'CIRCE', link: 'https://forms.gle/jvBGLctuce7UJYY76' },
-  { name: 'NAIADS', link: 'https://forms.gle/Zp42F3XDwnebNbob6' },
-  { name: 'CHARYBDIS', link: 'https://forms.gle/mop7DaabwVZYv7169' },
-  { name: 'CALYPSO', link: 'https://forms.gle/2edmZVuzmpkLez9Q9' },
-  { name: 'HIPPOCAMPUS', link: 'https://forms.gle/ZsmzZJHBEFy3CqyQ6' },
-  { name: 'KRAKEN', link: 'https://forms.gle/B2EYU5QS7iJJW9g39' },
-  { name: 'LEVIATHAN', link: 'https://forms.gle/x7SByULMDV9y7u3M6' },
-  { name: 'NEREUS', link: 'https://forms.gle/z9mKqqb289WmYmpX9' },
-  { name: 'HYDRA', link: 'https://forms.gle/ENtiYAQ6fhsd4hGL8' },
-  { name: 'CETEA', link: 'https://forms.gle/tq6rpXQKG2RxaxVt8' },
-  { name: 'SCYLLA', link: 'https://forms.gle/3jMAfuvKcHYrvQMw9' },
+  {
+    name: 'SIRENS',
+    link: 'https://forms.gle/h96U2s9qztsvdQ3H9',
+    members: [
+      'Kenneth Wilbert S',
+      'Vincent Alexander',
+      'Jesselyn Natania',
+      'Janice Auryn',
+      'Celica',
+      'Chelsea Valencia Chairil',
+      'Eunike Ardaya Pradipta Rukma',
+      'Felicia Grainne',
+      'Stevani',
+      'Clarissa Beatrice',
+    ],
+  },
+  {
+    name: 'CIRCE',
+    link: 'https://forms.gle/jvBGLctuce7UJYY76',
+    members: [
+      'Sesilia Brenda',
+      'Natania Angela',
+      'Ivan Satria',
+      'Angelia Sean',
+      'Jose Armando Christian',
+      'Cecillia Muliawan',
+      'Keisha Nathanie',
+      'Aurellia Ellin Ko',
+      'Felicia Viana Lim',
+      'Vallerie Ophelia Chavella',
+      'Eveline',
+      'Christabel Orleans Setiaji Adiwana',
+    ],
+  },
+  {
+    name: 'NAIADS',
+    link: 'https://forms.gle/Zp42F3XDwnebNbob6',
+    members: [
+      'Evi Febriyanti',
+      'Elvia Phedha Virajayo',
+      'Owen Montana',
+      'Elmo Chang You Shen',
+      'Frederico Jonathan Baguna',
+      'Eunike Diva Yusuf',
+      'Fulgentius Filbert Suryawan',
+      'Enna Fiesta',
+      'Yohanes Govinda Agarota',
+      'Sakti Yudha Pratama',
+      'Alexander',
+    ],
+  },
+  {
+    name: 'CHARYBDIS',
+    link: 'https://forms.gle/mop7DaabwVZYv7169',
+    members: [
+      'Joy Reuben Christian',
+      'Yosafat Benedictus Manik',
+      'Ficelia Winata',
+      'Aloysius Fidelio Wola',
+      'Kevin Ernando',
+      'Gilbert Stefanus Candra',
+      'Graciell Syallom Zefanya Tjandra',
+      'Nelson Efrata Aliwarga',
+    ],
+  },
+  {
+    name: 'CALYPSO',
+    link: 'https://forms.gle/2edmZVuzmpkLez9Q9',
+    members: [
+      'Janice Stefani',
+      'Kezia Filandi Wijaya',
+      'Najwa Aulia',
+      'Kenneth Welnysun',
+      'Brilliana Thalia Wijaya',
+    ],
+  },
+  {
+    name: 'HIPPOCAMPUS',
+    link: 'https://forms.gle/ZsmzZJHBEFy3CqyQ6',
+    members: [
+      'Brigitta Vania Maharani',
+      'Vivian Regina Surya',
+      'Valencia Antoinette',
+      'Kenizyanadi Viede Putri Hananda',
+      'Willy Wijaya Krisrena Cong',
+      'Marcel Lim',
+    ],
+  },
+  {
+    name: 'KRAKEN',
+    link: 'https://forms.gle/B2EYU5QS7iJJW9g39',
+    members: [
+      'Edlyn Louisa Wijaya',
+      'Laura Novelia Pakpahan',
+      'Rachel Manurung',
+      'Reginald Ulysses Lorenzo. S',
+      'Vanessa Angelica Hotama',
+    ],
+  },
+  {
+    name: 'LEVIATHAN',
+    link: 'https://forms.gle/x7SByULMDV9y7u3M6',
+    members: ['Amanda Candrasaputra', 'Debora Esterlita', 'Nicole Clement'],
+  },
+  {
+    name: 'NEREUS',
+    link: 'https://forms.gle/z9mKqqb289WmYmpX9',
+    members: [
+      'Wahyu Dwi Setio Wibowo',
+      'Damar Nur Rafly',
+      'Valentino Mahardhika Putra',
+      'Hans Philemon Limanza',
+    ],
+  },
+  {
+    name: 'HYDRA',
+    link: 'https://forms.gle/ENtiYAQ6fhsd4hGL8',
+    members: [
+      'Calista Belva',
+      'Alexander Bryan Triharja',
+      'Felix Sanuel Leo',
+      'Samuel Kurniawan Chandra',
+    ],
+  },
+  {
+    name: 'CETEA',
+    link: 'https://forms.gle/tq6rpXQKG2RxaxVt8',
+    members: [
+      'Carissa Alexandra Geraldi',
+      'Chintia Libby',
+      'Aurell Vienca Yudhisti',
+      'Felicia Alexandra',
+      'Kidung Kirana Ardyanto',
+      'Jeanne Annabelle Benz Pamungkas',
+      'Jesslyn Angelina',
+    ],
+  },
+  {
+    name: 'SCYLLA',
+    link: 'https://forms.gle/3jMAfuvKcHYrvQMw9',
+    members: [
+      'Jantzen Fernandez',
+      'Grisvian Revaldo',
+      'Valecia Monroe Thendy',
+      'Aretha Thalia Putri',
+      'Louis Rio Christiano',
+      'Jessie Yenca Vegustin',
+      'Angelica Michelle Sunarno',
+      'Sayed Aqbil Ruhullya Muntazhar',
+      'Tresya Meisel Adieputri',
+      'Amelia Fathjria Anugrah Akbar',
+      'Fabio',
+      'Mario Susanto',
+      'Davin Nathanael Ruslim',
+    ],
+  },
 ];
 
 const loadImage = (entries) => {
@@ -20,8 +164,14 @@ const loadImage = (entries) => {
       require(`@/asset/images/pages/recruitment/star/${entry.name}-Star.png`).default.src;
     entry.button =
       require(`@/asset/images/pages/recruitment/button/${entry.name}.png`).default.src;
-    entry.popup =
-      require(`@/asset/images/pages/recruitment/popup/${entry.name}-min.png`).default.src;
+    entry.popupd =
+      require(`@/asset/images/pages/recruitment/popup/desktop/${entry.name}-min.png`).default.src;
+    entry.popupdbg =
+      require(`@/asset/images/pages/recruitment/popup/desktop/bg/${entry.name}-bg.png`).default.src;
+    entry.popupm =
+      require(`@/asset/images/pages/recruitment/popup/mobile/${entry.name}-mobile.png`).default.src;
+    entry.popupmbg =
+      require(`@/asset/images/pages/recruitment/popup/mobile/bg/${entry.name}-bg-m.png`).default.src;
   });
 };
 
--- src/pages/recruitment/popup.js
+++ src/pages/recruitment/popup.js
@@ -1,28 +1,234 @@
 import { useEffect, useState } from 'react';
-import { Box, Flex, AspectRatio, Image } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  AspectRatio,
+  Image,
+  Wrap,
+  WrapItem,
+  keyframes,
+  Text,
+} from '@chakra-ui/react';
+
+//dynamic window dimensions checker
+function getWindowsDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return { width, height };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowsDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowsDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
 
 const Popup = (props) => {
   useEffect(() => {
     document.getElementById('popup').focus();
   });
 
-  // // Count Down (semoga berhasil :p -> update suda berhasil yey)
-  // const [currDate, setCurrDate] = useState(new Date().getTime());
-  // var startDate = new Date('2023-03-01').setHours(0); // midnight
+  const { height, width } = useWindowDimensions();
+  const [revealed, setRevealed] = useState(0);
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setCurrDate(new Date().getTime());
-  //     // console.log("Date : " + currDate);
-  //   }, 1000);
-  //   return() => clearInterval(interval);
-  // },[])
-  // // End Count Down
+  const fade = keyframes`from {opacity: 0%}; to {opacity: 100%}`;
 
+  if (props.selectedDivisi.name !== 'POSEIDON') {
+    var listNama = props.selectedDivisi.members.map((member, idx) => {
+      return (
+        <WrapItem
+          key={idx}
+          animation={`${fade} ${
+            Math.floor(Math.random() * 3) + Math.floor(Math.random() * 5)
+          }s linear`}
+        >
+          <Text fontSize={height > width ? ['2vw', '2.5vw'] : ['3vw', '1.3vw']}>
+            {member}
+          </Text>
+        </WrapItem>
+      );
+    });
+
+    // console.log(listNama);
+    // // Count Down (semoga berhasil :p -> update suda berhasil yey)
+    // const [currDate, setCurrDate] = useState(new Date().getTime());
+    // var startDate = new Date('2023-03-01').setHours(0); // midnight
+
+    // useEffect(() => {
+    //   const interval = setInterval(() => {
+    //     setCurrDate(new Date().getTime());
+    //     // console.log("Date : " + currDate);
+    //   }, 1000);
+    //   return() => clearInterval(interval);
+    // },[])
+    // // End Count Down
+
+    return (
+      <Flex
+        w="100%"
+        h="100%"
+        pos="absolute"
+        bgColor="rgba(0,0,0,0.7)"
+        onClick={() => props.setPopUp('false')}
+        justifyContent="center"
+        alignItems="center"
+        p={0}
+        m={0}
+        top={0}
+        flexDirection="column"
+        tabIndex={0}
+        id="popup"
+      >
+        {height > width ? (
+          <AspectRatio ratio={1191 / 1645} w={'75%'}>
+            {revealed ? (
+              <Flex
+                bgImage={props.selectedDivisi.popupmbg}
+                w="75%"
+                bgSize="contain"
+                bgPos="center"
+                bgRepeat="no-repeat"
+                onClick={(e) => {
+                  if (!e) var e = window.event;
+                  e.cancelBubble = true;
+                  if (e.stopPropagation) e.stopPropagation();
+                  setRevealed(0);
+                  // setOpacity(0);
+                }}
+                // animation={
+                //   opacity ? `${fade} 2s linear` : `${fade} 2s linear reverse`
+                // }
+                // onAnimationEnd={() => {
+                //   if (revealed) setRevealed(0);
+                // }}
+              >
+                <Wrap
+                  w="70%"
+                  position="absolute"
+                  top="50%"
+                  align="center"
+                  justify="space-evenly"
+                >
+                  {listNama}
+                </Wrap>
+              </Flex>
+            ) : (
+              <Image
+                src={props.selectedDivisi.popupm}
+                placeholder="blur"
+                layout="fill"
+                draggable="false"
+                onClick={(e) => {
+                  if (!e) var e = window.event;
+                  e.cancelBubble = true;
+                  if (e.stopPropagation) e.stopPropagation();
+                  setRevealed(1);
+                  // setOpacity(1);
+                }}
+                // animation={
+                //   opacity ? `${fade} 2s linear reverse` : `${fade} 2s linear`
+                // }
+                // onAnimationEnd={() => {
+                //   if (!revealed) setRevealed(1);
+                // }}
+              />
+            )}
+          </AspectRatio>
+        ) : (
+          <AspectRatio ratio={1546 / 1191} w={['90%', '75%', '65%', '50%']}>
+            {revealed ? (
+              <Flex
+                bgImage={props.selectedDivisi.popupdbg}
+                w="75%"
+                bgSize="contain"
+                bgPos="center"
+                bgRepeat="no-repeat"
+                onClick={(e) => {
+                  if (!e) var e = window.event;
+                  e.cancelBubble = true;
+                  if (e.stopPropagation) e.stopPropagation();
+                  setRevealed(0);
+                  // setOpacity(0);
+                }}
+                // animation={
+                //   opacity ? `${fade} 2s linear` : `${fade} 2s linear reverse`
+                // }
+                // onAnimationEnd={() => {
+                //   if (revealed) setRevealed(0);
+                // }}
+              >
+                <Wrap
+                  w="70%"
+                  position="absolute"
+                  top="50%"
+                  align="center"
+                  justify="space-evenly"
+                >
+                  {listNama}
+                </Wrap>
+              </Flex>
+            ) : (
+              <Image
+                src={props.selectedDivisi.popupd}
+                placeholder="blur"
+                layout="fill"
+                draggable="false"
+                onClick={(e) => {
+                  if (!e) var e = window.event;
+                  e.cancelBubble = true;
+                  if (e.stopPropagation) e.stopPropagation();
+                  setRevealed(1);
+                }}
+              />
+            )}
+          </AspectRatio>
+        )}
+
+        {/* BUTTON PENDAFTARAN */}
+        {/* {props.selectedDivisi.name !== 'POSEIDON' && (
+          <AspectRatio
+            ratio={732 / 150}
+            w={['45%', '30%', '25%', '20%']}
+            my="20px"
+            _hover={{
+              cursor: 'pointer',
+              transform: 'scale(1.1)',
+            }}
+            transition=".3s ease-in-out"
+          >
+            <Box
+              bgImage={props.selectedDivisi.button}
+              w="70%"
+              bgSize="contain"
+              bgPos="center"
+              bgRepeat="no-repeat"
+              onClick={(e) => {
+                if (!e) var e = window.event;
+                e.cancelBubble = true;
+                if (e.stopPropagation) e.stopPropagation();
+                window.open(props.selectedDivisi.link);
+              }}
+            />
+          </AspectRatio>
+        )} */}
+      </Flex>
+    );
+  }
+
+  //POSEIDON
   return (
     <Flex
       w="100%"
-      // h={['35%', '42.5%', '70%', '70%']}
       h="100%"
       pos="absolute"
       bgColor="rgba(0,0,0,0.7)"
@@ -36,55 +242,35 @@ const Popup = (props) => {
       tabIndex={0}
       id="popup"
     >
-      <AspectRatio ratio={1546 / 1191} w={['90%', '75%', '65%', '50%']}>
-        {/* <Box
-          bgImage={props.selectedDivisi.popup}
-          w="100%"
-          bgSize="contain"
-          bgPos="center"
-          bgRepeat="no-repeat"
-        /> */}
-        <Image
-          src={props.selectedDivisi.popup}
-          placeholder="blur"
-          layout="fill"
-          draggable="false"
-          onClick={(e) => {
-            if (!e) var e = window.event;
-            e.cancelBubble = true;
-            if (e.stopPropagation) e.stopPropagation();
-          }}
-        />
-      </AspectRatio>
-      
-      {/* BUTTON PENDAFTARAN */}
-      {/* {props.selectedDivisi.name !== 'POSEIDON' && currDate >= startDate && ( */}
-      {/* {props.selectedDivisi.name !== 'POSEIDON' && (
-        <AspectRatio
-          ratio={732 / 150}
-          w={['45%', '30%', '25%', '20%']}
-          my="20px"
-          _hover={{
-            cursor: 'pointer',
-            transform: 'scale(1.1)',
-          }}
-          transition=".3s ease-in-out"
-        >
-          <Box
-            bgImage={props.selectedDivisi.button}
-            w="70%"
-            bgSize="contain"
-            bgPos="center"
-            bgRepeat="no-repeat"
+      {height > width ? (
+        <AspectRatio ratio={1191 / 1645} w={'75%'}>
+          <Image
+            src={props.selectedDivisi.popupm}
+            placeholder="blur"
+            layout="fill"
+            draggable="false"
             onClick={(e) => {
               if (!e) var e = window.event;
               e.cancelBubble = true;
               if (e.stopPropagation) e.stopPropagation();
-              window.open(props.selectedDivisi.link);
             }}
           />
         </AspectRatio>
-      )} */}
+      ) : (
+        <AspectRatio ratio={1546 / 1191} w={['90%', '75%', '65%', '50%']}>
+          <Image
+            src={props.selectedDivisi.popupd}
+            placeholder="blur"
+            layout="fill"
+            draggable="false"
+            onClick={(e) => {
+              if (!e) var e = window.event;
+              e.cancelBubble = true;
+              if (e.stopPropagation) e.stopPropagation();
+            }}
+          />
+        </AspectRatio>
+      )}
     </Flex>
   );
 };
