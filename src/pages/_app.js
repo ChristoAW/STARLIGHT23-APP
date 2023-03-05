6f49e0ecdb74f44e3833bb0bbc66d09c4444dd98
ChristoAW <99455915+ChristoAW@users.noreply.github.com>
2023-03-05 12:03:07 +0700

    fix: splashscreen

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ const BaseHead = () => {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content="Let's Sail To The Horizon" />
-      
+
       <link rel="icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" href="/logo/starlight.png" />
     </Head>
@@ -40,7 +40,7 @@ function MyApp({ Component, pageProps }) {
       <ChakraProvider theme={theme}>
         <LayoutProvider>
           {/* <Scroll /> */}
-          {/* <SplashScreen /> */}
+          <SplashScreen />
           <Component {...pageProps} />
         </LayoutProvider>
       </ChakraProvider>
