02db633b09d12e0df04bb25f5f922834aef2ee13
maecyntha <maecyntha.tan@gmail.com>
2023-05-11 18:13:32 +0700

    fix fix
    ~ zIndex for arrow in latest news
    ~ lowercase division name for colors.js

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -23,20 +23,35 @@ const colors = {
   },
   
   div: {
-    CALYPSO: "#a7bf46",
-    CETEA: "#845f3e",
-    CHARYBDIS: "#5e8eca",
-    CIRCE: "#9e3a3a",
-    HIPPOCAMPUS: "#2d9d4a",
-    HYDRA: "#d3934a",
-    KRAKEN: "#43bd8b",
-    LEVIATHAN: "#56bdd7",
-    NAIADS: "#9680c3",
-    NEREUS: "#e0c858",
-    POSEIDON: "#edece9",
-    SCYLLA: "#3f3f3f",
-    SIRENS: "#c27eb8",
+    Calypso: "#a7bf46",
+    Cetea: "#845f3e",
+    Charybdis: "#5e8eca",
+    Circe: "#9e3a3a",
+    Hippocampus: "#2d9d4a",
+    Hydra: "#d3934a",
+    Kraken: "#43bd8b",
+    Leviathan: "#56bdd7",
+    Naiads: "#9680c3",
+    Nereus: "#e0c858",
+    Poseidon: "#edece9",
+    Scylla: "#3f3f3f",
+    Sirens: "#c27eb8",
   }
+  // div: {
+  //   CALYPSO: "#a7bf46",
+  //   CETEA: "#845f3e",
+  //   CHARYBDIS: "#5e8eca",
+  //   CIRCE: "#9e3a3a",
+  //   HIPPOCAMPUS: "#2d9d4a",
+  //   HYDRA: "#d3934a",
+  //   KRAKEN: "#43bd8b",
+  //   LEVIATHAN: "#56bdd7",
+  //   NAIADS: "#9680c3",
+  //   NEREUS: "#e0c858",
+  //   POSEIDON: "#edece9",
+  //   SCYLLA: "#3f3f3f",
+  //   SIRENS: "#c27eb8",
+  // }
 };
 
 export default colors;
