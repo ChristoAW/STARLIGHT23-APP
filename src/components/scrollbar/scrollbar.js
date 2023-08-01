b4ab872fc5b01892531c0d0415ac2d0c111b898b
Tinosz <valentinomahardhikaputra@gmail.com>
2023-08-01 20:08:04 +0700

    Add Scrollbar Interactivity
    Scrollbar is still enabled

--- src/components/scrollbar/scrollbar.js
+++ src/components/scrollbar/scrollbar.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import styles from './scrollbar.module.css';
 
@@ -8,6 +8,10 @@ import Image from 'next/image';
 
 const Scrollbar = () => {
   const [scrollProgress, setScrollProgress] = useState(0);
+  const [dragging, setDragging] = useState(false);
+  const [startY, setStartY] = useState(0);
+  const anchorRef = useRef(null);
+
   useEffect(() => {
     const handleScroll = () => {
       const totalScrollHeight =
@@ -23,17 +27,76 @@ const Scrollbar = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+  if (dragging) {
+    const maxScrollHeight =
+      document.documentElement.scrollHeight - window.innerHeight;
+    const offsetY = event.clientY - startY;
+    const scrollPercentage = (offsetY / window.innerHeight) * 100;
+    const newScrollProgress = Math.max(0, Math.min(scrollPercentage, maxScrollHeight));
+
+    const minScrollProgress = (72.3667 / window.innerHeight) * 100;
+    const maxScrollProgress = 100;
+    const clampedScrollProgress = Math.max(minScrollProgress, Math.min(newScrollProgress, maxScrollProgress));
+
+    setScrollProgress(clampedScrollProgress);
+
+    const newScrollY = (clampedScrollProgress / 100) * (document.documentElement.scrollHeight - window.innerHeight);
+    window.scrollTo(0, newScrollY);
+  }
+};
+
+
+    const handleMouseUp = () => {
+      setDragging(false);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('mouseup', handleMouseUp);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [dragging, startY]);
+
+  const handleMouseDown = (event) => {
+    event.preventDefault();
+    setDragging(true);
+    const boundingClientRect = anchorRef.current.getBoundingClientRect();
+    const initialDisplacement =
+      scrollProgress - (72.3667 / window.innerHeight) * 100;
+    setStartY(event.clientY - boundingClientRect.top - initialDisplacement);
+  };
+  
+
   const checkpoints = [0, 20, 40, 60, 80];
-  const numChains = checkpoints.filter(checkpoints => scrollProgress >= checkpoints).length;
+  const numChains = checkpoints.filter((checkpoints) => scrollProgress >= checkpoints).length;
+
+  const maxTranslateY = scrollProgress - (72.3667 / window.innerHeight) * 100;
 
   return (
     <div>
-        {Array.from({length: numChains}, (_,index) => (
-            <Image key={index} src={Chain} className={styles.scrollbar} style={{
-                transform: `translateY(calc(${scrollProgress}vh - ${index * 192.717 + 192.717}px))`,
-              }} />
-        ))}
-      <Image src={Anchor} className={styles.scrollbar} style={{ transform: `translateY(${scrollProgress}vh)` }}/>
+      {Array.from({ length: numChains }, (_, index) => (
+        <Image
+          key={index}
+          src={Chain}
+          className={styles.scrollbar}
+          style={{
+            transform: `translateY(calc(${scrollProgress}vh - ${index * 192.717 + 192.717 + 72.3667}px))`,
+          }}
+        />
+      ))}
+      <Image
+        ref={anchorRef} 
+        src={Anchor}
+        className={styles.scrollbar}
+        style={{
+          transform: `translateY(calc(${maxTranslateY}vh))`,
+          cursor: 'grab'
+        }}
+        onMouseDown={handleMouseDown}
+      />
     </div>
   );
 };
