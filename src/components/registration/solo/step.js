8797f7db18c44a2c5ac3bf7a1a0a78e3c7dfdf94
maecyntha <maecyntha.tan@gmail.com>
2023-08-18 17:03:45 +0700

    update: step.js (solo)

--- src/components/registration/solo/step.js
+++ src/components/registration/solo/step.js
@@ -1,6 +1,9 @@
 import { VStack, Button, Flex, Link, Text, Box } from '@chakra-ui/react';
-import { useForm } from 'react-hook-form';
 import theme from '@/theme';
+
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
 
 import {
   FormBox,
@@ -20,8 +23,6 @@ import {
   FormInputEmail,
 } from '../styles';
 
-import { useState } from 'react';
-
 import { storage } from '@/pages/api/isthara/firebase';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { v4 } from 'uuid';
@@ -30,6 +31,8 @@ function soloForm() {
   const { setValue, handleSubmit } = useForm();
   const [twibbonUpload, setTwibbonUpload] = useState(null);
   const [instagramUpload, setInstagramUpload] = useState(null);
+
+  const router = useRouter();
 
   // this state will be used to store all required data except for those that has its own state
   const [formValue, setFormValue] = useState({
@@ -49,12 +52,13 @@ function soloForm() {
     setValue('timestamp', new Date().toLocaleString() + '');
     setValue('stageName', formValue.stageName);
     setValue('name', formValue.name);
-    setValue('nim', formValue.nim);
+    setValue('nim', "'000000" + formValue.nim);
     setValue('univ', formValue.univ);
     setValue('tel', formValue.tel);
     setValue('instagram', formValue.instagram);
     setValue('email', formValue.email);
     // handle the remaining values
+    // image upload handler is inside the fileHandler function
 
     // reset all input fields
     //
@@ -66,17 +70,7 @@ function soloForm() {
     setFormValue(data);
   };
 
-  async function submitHandler(data) {
-    const response = await fetch('/api/isthara/solo', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    console.log(response.status, response.statusText);
-    console.log(formValue);
-
+  async function fileHandler() {
     // Upload the files to Firebase Storage
     if (twibbonUpload !== null) {
       const twibbonRef = ref(
@@ -89,6 +83,7 @@ function soloForm() {
       //link twibbon url
       const twibbonDownloadURL = await getDownloadURL(twibbonRef);
       console.log('Twibbon Image:', twibbonDownloadURL);
+      setValue('twibProof', twibbonDownloadURL);
     }
 
     if (instagramUpload !== null) {
@@ -102,7 +97,22 @@ function soloForm() {
       //link proof instagram url
       const instagramDownloadURL = await getDownloadURL(instagramRef);
       console.log('Instagram Image:', instagramDownloadURL);
+      setValue('igProof', instagramDownloadURL);
     }
+  }
+
+  async function submitHandler(data) {
+    await fileHandler();
+
+    const response = await fetch('/api/isthara/solo', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log(response.status, response.statusText);
+    // console.log(formValue);
 
     // Disini untuk reset semua input setelah masuk ke sheet
     setValue('name', '');
@@ -112,106 +122,105 @@ function soloForm() {
     setValue('tel', '');
     setValue('instagram', '');
     setValue('email');
+
+    router.push('/registration/ishtaraReg/welcome')
   }
 
   return (
-    <VStack>
-      <FormBox>
-        <form onSubmit={handleSubmit(submitHandler)}>
-          <FormHeading>
-            Pendaftaran I<i>s</i>htara <i>S</i>tarlight 2023
-          </FormHeading>
-          <FormSubHeading>Solo Registration</FormSubHeading>
-          <Box
-            textAlign="center"
-            fontSize={{ base: '1rem', md: '1.25rem', sm: '.75rem' }}
-          >
-            <Text as="b">Contact Person:</Text>
-            <Text>
-              Chintia Libby <br />
-              +62 815 1073 7353 (Whatsapp)
-            </Text>
-          </Box>
-          <FormTextImportant>Full Name</FormTextImportant>
-          <FormInputImportant
-            placeholder="Chintia Libby"
-            name="name"
-            onChange={(event) => handleChange(event)}
-          >
-            Error: Field cannot be empty
-          </FormInputImportant>
+    <form onSubmit={handleSubmit(submitHandler)}>
+      <FormBox mt={0}>
+        <FormHeading>
+          Pendaftaran I<i>s</i>thara <i>S</i>tarlight 2023
+        </FormHeading>
+        <FormSubHeading>Solo Registration</FormSubHeading>
+        <Box
+          textAlign="center"
+          fontSize={{ base: '1rem', md: '1.25rem', sm: '.75rem' }}
+        >
+          <Text as="b">Contact Person:</Text>
+          <Text>
+            Chintia Libby <br />
+            +62 815 1073 7353 (Whatsapp)
+          </Text>
+        </Box>
+        <FormTextImportant>Full Name</FormTextImportant>
+        <FormInputImportant
+          placeholder="Chintia Libby"
+          name="name"
+          onChange={(event) => handleChange(event)}
+        >
+          Error: Field cannot be empty
+        </FormInputImportant>
 
-          <FormTextImportant>Stage Name</FormTextImportant>
-          <FormInputImportant
-            placeholder="Stage Name"
-            name="stageName"
-            onChange={(event) => handleChange(event)}
-          >
-            Error: Field cannot be empty
-          </FormInputImportant>
-          <FormTextImportant>University</FormTextImportant>
-          <FormInputImportant
-            placeholder="Universitas Multimedia Nusantara"
-            name="univ"
-            onChange={(event) => handleChange(event)}
-          >
-            Error: Field cannot be empty
-          </FormInputImportant>
-          <FormText>Nomor Induk Mahasiswa (NIM)</FormText>
-          <FormInputNIM
-            name="nim"
-            onChange={(event) => handleChange(event)}
-            placeholder="12345"
-          >
-            Please Enter Only Numbers
-          </FormInputNIM>
-          <FormNotes>For UMN Students Only</FormNotes>
-          <FormTextImportant>Phone Number (Whatsapp)</FormTextImportant>
-          <FormInputTel
-            placeholder="0812345678"
-            name="tel"
-            onChange={(event) => handleChange(event)}
-          >
-            Error: Please input (+62 or 0) followed by 7-13 numbers
-          </FormInputTel>
-          <FormNotes>Example: +62XXXXXXXXXX</FormNotes>
-          <FormTextImportant>Instagram</FormTextImportant>
-          <FormInputImportant
-            placeholder="@starlightumn"
-            name="instagram"
-            onChange={(event) => handleChange(event)}
-          >
-            Error: Field cannot be empty
-          </FormInputImportant>
-          <FormTextImportant>Email</FormTextImportant>
-          <FormInputEmail
-            placeholder="starlight@umn.ac.id"
-            name="email"
-            onChange={(event) => handleChange(event)}
-          />
-          {/*🔻File Upload disini🔻*/}
-          <FormTextImportant>Proof of Uploading Twibbon</FormTextImportant>
-          <Link href="twibbon" color={theme.colors.text[600]}>
-            *Twibbon Link Here*
-          </Link>
+        <FormTextImportant>Stage Name</FormTextImportant>
+        <FormInputImportant
+          placeholder="Stage Name"
+          name="stageName"
+          onChange={(event) => handleChange(event)}
+        >
+          Error: Field cannot be empty
+        </FormInputImportant>
+        <FormTextImportant>University</FormTextImportant>
+        <FormInputImportant
+          placeholder="Universitas Multimedia Nusantara"
+          name="univ"
+          onChange={(event) => handleChange(event)}
+        >
+          Error: Field cannot be empty
+        </FormInputImportant>
+        <FormText>Nomor Induk Mahasiswa (NIM)</FormText>
+        <FormInputNIM
+          name="nim"
+          onChange={(event) => handleChange(event)}
+          placeholder="12345"
+        >
+          Please Enter Only Numbers
+        </FormInputNIM>
+        <FormNotes>For UMN Students Only</FormNotes>
+        <FormTextImportant>Phone Number (Whatsapp)</FormTextImportant>
+        <FormInputTel
+          placeholder="0812345678"
+          name="tel"
+          onChange={(event) => handleChange(event)}
+        >
+          Error: Please input (+62 or 0) followed by 7-13 numbers
+        </FormInputTel>
+        <FormNotes>Example: +62XXXXXXXXXX</FormNotes>
+        <FormTextImportant>Instagram</FormTextImportant>
+        <FormInputImportant
+          placeholder="@starlightumn"
+          name="instagram"
+          onChange={(event) => handleChange(event)}
+        >
+          Error: Field cannot be empty
+        </FormInputImportant>
+        <FormTextImportant>Email</FormTextImportant>
+        <FormInputEmail
+          placeholder="starlight@umn.ac.id"
+          name="email"
+          onChange={(event) => handleChange(event)}
+        />
+        {/*🔻File Upload disini🔻*/}
+        <FormTextImportant>Proof of Uploading Twibbon</FormTextImportant>
+        <Link href="twibbon" color={theme.colors.text[600]}>
+          *Twibbon Link Here*
+        </Link>
 
-          <FormInputFile
-            name="twibbon"
-            onChange={(event) => {
-              setTwibbonUpload(event.target.files[0]);
-            }}
-          />
-          <FormTextImportant>
-            Proof of Following @starlight.umn
-          </FormTextImportant>
-          <FormInputFile
-            name="instagram"
-            onChange={(event) => {
-              setInstagramUpload(event.target.files[0]);
-            }}
-          />
+        <FormInputFile
+          name="twibbon"
+          onChange={(event) => {
+            setTwibbonUpload(event.target.files[0]);
+          }}
+        />
+        <FormTextImportant>Proof of Following @starlight.umn</FormTextImportant>
+        <FormInputFile
+          name="instagram"
+          onChange={(event) => {
+            setInstagramUpload(event.target.files[0]);
+          }}
+        />
 
-          {/*<FormTextImportant>Description</FormTextImportant>
+        {/*<FormTextImportant>Description</FormTextImportant>
           <FormTextareaImportant
             placeholder="Performance description"
             name="desc"
@@ -223,28 +232,23 @@ function soloForm() {
             Error: Field cannot be empty
           </FormTextareaImportant>
           <FormNotes>Starlight will provide...</FormNotes> */}
-
-          <br />
-          <Flex justify="flex-end" maxW="1080px" w="100%" mx="auto" mb="2em">
-            <NextLink href="/registration/ishtaraReg/welcome" passHref>
-              <Link
-                h="3rem"
-                w="9rem"
-                color={theme.colors.deco[400]}
-                bgColor={theme.colors.bg[700]}
-                border="1px"
-                as={Button}
-                type="submit"
-                onClick={defineValue}
-                _hover={{ bgColor: '0,0,0', color: ' rgb(227,218,201)' }}
-              >
-                SUBMIT
-              </Link>
-            </NextLink>
-          </Flex>
-        </form>
       </FormBox>
-    </VStack>
+      <Flex justify="flex-end" maxW="1080px" w="100%" mx="auto" mb="2em">
+        <Link
+          h="3rem"
+          w="9rem"
+          color={theme.colors.deco[400]}
+          bgColor={theme.colors.bg[700]}
+          border="1px"
+          as={Button}
+          type="submit"
+          onClick={defineValue}
+          _hover={{ bgColor: '0,0,0', color: ' rgb(227,218,201)' }}
+        >
+          SUBMIT
+        </Link>
+      </Flex>
+    </form>
   );
 }
 
