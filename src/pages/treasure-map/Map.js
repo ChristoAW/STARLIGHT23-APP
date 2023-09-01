af25a421800651939577f1ddb0f7fba9affdb5ae
Achxter <hansphilemon@gmail.com>
2023-09-01 21:32:52 +0700

    responsive map

--- src/pages/treasure-map/Map.js
+++ src/pages/treasure-map/Map.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import { Flex, Box, Image, AspectRatio } from '@chakra-ui/react';
 import TM from "@/asset/images/treasure/TM_120 kosong.png"
 import AbyssOpen from "@/asset/images/treasure/Abyss Terbuka.png"
@@ -10,92 +10,261 @@ import ElysiumClose from "@/asset/images/treasure/Elysium_(cangkang_tutup)_REV.p
 
 
 function tm() {
-    return (
-        <AspectRatio
-            // w="100%"
-            w="100vh"
-            h="100vw"
-            ratio={16 / 9}
-            marginTop="78px"
-        >
-            <Flex
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    if (screenWidth >= 992) {
+        // Large screen (md and above)
+        return (
+            <AspectRatio
+                w="100%"
                 h="100%"
-                position="relative"
-                transform="rotate(90deg)"
+                ratio={16 / 9}
+                paddingBottom="16vh"
             >
-                <Image
-                    maxW="none"
-                    w="100vh"
-                    h="100vw"
-                    objectFit='cover'
-                    src={TM.src}
-                    alt=''
-                >
-                </Image>
-                <Box
-                    position="fixed"
-                    top="110px"
-                    left="120px"
+                <Flex
+                    h="100%"
+                    position="relative"
                 >
                     <Image
-                        boxSize={{ base: '20%', sm: "20%" }}
+                        maxW="none"
+                        w="100vw"
+                        h="initial"
                         objectFit='cover'
-                        src={AbyssOpen.src}
+                        src={TM.src}
                         alt=''
-                    />
-                    {/* <Image
-                        boxSize='20%'
-                        objectFit='cover'
-                        src={AbyssClose.src}
-                        alt=''
-                        marginTop="1rem"
-                    /> */}
-                </Box>
-                <Box
-                    position="fixed"
-                    top="200px"
-                    left="330px"
+                    >
+                    </Image>
+                    <Box
+                        position="absolute"
+                        bottom="27vw"
+                        left="16vw"
+                    >
+                        {/* <Image
+                            boxSize="18vw"
+                            objectFit='cover'
+                            src={AbyssOpen.src}
+                            alt=''
+                        /> */}
+                        <Image
+                            w="18vw"
+                            objectFit='cover'
+                            src={AbyssClose.src}
+                            alt=''
+                        />
+                    </Box>
+                    <Box
+                        position="absolute"
+                        bottom="19vw"
+                        left="41vw"
+                    >
+                        {/* <Image
+                            boxSize="18vw"
+                            objectFit='cover'
+                            src={LabyrssOpen.src}
+                            alt=''
+                        /> */}
+                        <Image
+                            w="18vw"
+                            objectFit='cover'
+                            src={LabyrssClose.src}
+                            alt=''
+                        />
+                    </Box>
+                    <Box
+                        position="absolute"
+                        bottom="33vw"
+                        left="65vw"
+                    >
+                        {/* <Image
+                            boxSize="18vw"
+                            objectFit='cover'
+                            src={ElysiumOpen.src}
+                            alt=''
+                        /> */}
+                        <Image
+                            w="18vw"
+                            objectFit='cover'
+                            src={ElysiumClose.src}
+                            alt=''
+                        />
+                    </Box>
+                </Flex>
+            </AspectRatio>
+        );
+    } else if (screenWidth >= 768) {
+        return (
+            <AspectRatio
+                w="100%"
+                h="100%"
+                ratio={16 / 9}
+                paddingBottom="16vh"
+            >
+                <Flex
+                    h="100%"
+                    position="relative"
                 >
                     <Image
-                        boxSize={{
-                            base: '25%',
-                            sm: "25%"
-                        }}
+                        maxW="none"
+                        w="100vw"
+                        h="initial"
                         objectFit='cover'
-                        src={LabyrssOpen.src}
+                        src={TM.src}
                         alt=''
-                    />
-                    {/* <Image
-                        boxSize='25%'
-                        objectFit='cover'
-                        src={LabyrssClose.src}
-                        alt=''
-                        marginTop="1.5rem"
-                    /> */}
-                </Box>
-                <Box
-                    position="fixed"
-                    top="60px"
-                    left="500px"
+                    >
+                    </Image>
+                    <Box
+                        position="absolute"
+                        bottom="33vw"
+                        left="16vw"
+                    >
+                        {/* <Image
+                            boxSize="18vw"
+                            objectFit='cover'
+                            src={AbyssOpen.src}
+                            alt=''
+                        /> */}
+                        <Image
+                            w="18vw"
+                            objectFit='cover'
+                            src={AbyssClose.src}
+                            alt=''
+                        />
+                    </Box>
+                    <Box
+                        position="absolute"
+                        bottom="26vw"
+                        left="43vw"
+                    >
+                        {/* <Image
+                            boxSize="18vw"
+                            objectFit='cover'
+                            src={LabyrssOpen.src}
+                            alt=''
+                        /> */}
+                        <Image
+                            w="18vw"
+                            objectFit='cover'
+                            src={LabyrssClose.src}
+                            alt=''
+                        />
+                    </Box>
+                    <Box
+                        position="absolute"
+                        bottom="39vw"
+                        left="65vw"
+                    >
+                        <Image
+                            boxSize="18vw"
+                            objectFit='cover'
+                            src={ElysiumOpen.src}
+                            alt=''
+                        />
+                        {/* <Image
+                            w="18vw"
+                            objectFit='cover'
+                            src={ElysiumClose.src}
+                            alt=''
+                        /> */}
+                    </Box>
+                </Flex>
+            </AspectRatio>
+        );
+    } else {
+        return (
+            <AspectRatio
+                w='100vh'
+                h='100vh'
+                ratio={16 / 9}
+                marginTop="0"
+                marginBottom="0"
+            >
+                <Flex
+                    h="100%"
+                    position="relative"
+                    transform='rotate(90deg)'
                 >
                     <Image
-                        boxSize={['45%', "40%"]}
+                        maxW="none"
+                        w='100vh'
+                        h='100vw'
                         objectFit='cover'
-                        src={ElysiumOpen.src}
+                        src={TM.src}
                         alt=''
-                    />
-                    {/* <Image
-                        boxSize='35%'
-                        objectFit='cover'
-                        src={ElysiumClose.src}
-                        alt=''
-                        marginStart="1rem"
-                        marginTop="3rem"
-                    /> */}
-                </Box>
-            </Flex>
-        </AspectRatio>
-    )
+                    >
+                    </Image>
+                    <Box
+                        position="absolute"
+                        bottom='44vh'
+                        left='16vh'
+                    >
+                        {/* <Image
+                            boxSize='35vw'
+                            objectFit='cover'
+                            src={AbyssOpen.src}
+                            alt=''
+                        /> */}
+                        <Image
+                            w='35vw'
+                            objectFit='cover'
+                            src={AbyssClose.src}
+                            alt=''
+                            marginTop="4vh"
+                        />
+                    </Box>
+                    <Box
+                        position="absolute"
+                        bottom='36vh'
+                        left='42vh'
+                    >
+                        {/* <Image
+                            boxSize='35vw'
+                            objectFit='cover'
+                            src={LabyrssOpen.src}
+                            alt=''
+                        /> */}
+                        <Image
+                            w='35vw'
+                            objectFit='cover'
+                            src={LabyrssClose.src}
+                            alt=''
+                            marginTop='6vh'
+                        />
+                    </Box>
+                    <Box
+                        position="absolute"
+                        bottom='50vh'
+                        right='18vh'
+                    >
+                        {/* <Image
+                                boxSize="35vw"
+                                objectFit='cover'
+                                src={ElysiumOpen.src}
+                                alt=''
+                            /> */}
+                        <Image
+                            w='35vw'
+                            objectFit='cover'
+                            src={ElysiumClose.src}
+                            alt=''
+                            marginTop='8vh'
+                        />
+                    </Box>
+                </Flex>
+            </AspectRatio>
+        );
+    }
 }
 
 export default tm
